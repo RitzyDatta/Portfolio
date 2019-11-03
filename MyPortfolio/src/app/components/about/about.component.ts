b3c7867e26cf1bb6efd6290ae8e64fa422460c6b
Rituparna Datta <ritzy24datta@gmail.com>
2019-11-03 20:18:38 +0530

    adding feature

--- MyPortfolio/src/app/components/about/about.component.ts
+++ MyPortfolio/src/app/components/about/about.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  animations: [
+    trigger('fade',[
+      transition('')
+
+    ])
+  ]
 })
 export class AboutComponent implements OnInit {
 
